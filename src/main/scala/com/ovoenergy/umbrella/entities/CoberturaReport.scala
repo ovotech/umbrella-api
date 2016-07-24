5bd63a242b8f6f46123038769ea9bfc7654d51e9
Stuart Izon <stuart.izon@ovoenergy.com>
2016-07-24 11:03:08 +0100

    Replaced unmarshaller with one that can handle the DOCTYPE line

--- src/main/scala/com/ovoenergy/umbrella/entities/CoberturaReport.scala
+++ src/main/scala/com/ovoenergy/umbrella/entities/CoberturaReport.scala
@@ -1,23 +1,23 @@
 package com.ovoenergy.umbrella.entities
 
 import org.joda.time.DateTime
-import spray.http.MediaTypes
+import spray.http.{HttpEntity, MediaTypes}
+import spray.httpx.unmarshalling.UnmarshallerLifting._
 import spray.httpx.unmarshalling._
 
-import scala.xml.NodeSeq
-
-import UnmarshallerLifting._
+import scala.xml.XML
 
 case class CoberturaReport(coverage: Float, date: DateTime, packages: List[(String, Float)])
 
 object CoberturaReport {
   val unmarshaller = fromRequestUnmarshaller {
     fromMessageUnmarshaller {
-      Unmarshaller.delegate[NodeSeq, CoberturaReport](MediaTypes.`application/xml`) {
-        nodeSeq =>
-          val coverage = (nodeSeq \@ "line-rate" toFloat) * 100
-          val date = new DateTime(nodeSeq \@ "timestamp" toLong)
-          val packages = (nodeSeq \ "packages" \ "package" toList) map {
+      Unmarshaller[CoberturaReport](MediaTypes.`application/xml`) {
+        case HttpEntity.NonEmpty(contentType, data) =>
+          val report = XML.loadString(data.asString(contentType.charset))
+          val coverage = (report \@ "line-rate" toFloat) * 100
+          val date = new DateTime(report \@ "timestamp" toLong)
+          val packages = (report \ "packages" \ "package" toList) map {
             node => (node \@ "name", (node \@ "line-rate" toFloat) * 100)
           }
           CoberturaReport(coverage, date, packages)
